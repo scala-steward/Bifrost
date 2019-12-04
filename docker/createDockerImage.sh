da30890b308480cc2a6283e1ec1d8c9c66a69aba
tuxman <n.edmonds@topl.me>
2019-12-04 14:03:22 -0600

    Update docker to create and push image

--- docker/createDockerImage.sh
+++ docker/createDockerImage.sh
@@ -1,3 +1,11 @@
-cp ../target/scala-2.12/project-bifrost-assembly-0.2.0-alpha-public.jar ./
+#!/usr/bin/env bash
 
-docker build -t toplprotocol/projectbifrost:0.2.0-testnet .
+cd ..
+
+sbt assembly
+
+cp target/scala-2.12/bifrost-assembly-3.0.0.jar docker/
+
+cd docker || exit
+
+docker build -t toplprotocol/bifrost:3.0.0 .
